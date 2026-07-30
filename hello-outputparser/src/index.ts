4b07aef8e176e67362867efe87fa4538ebfae075
江鑫 <jx3313@163.com>
2026-07-30 15:10:08 +0800

    feat: 添加 hello-runnabled 包及其配置文件，更新 pnpm 工作区以包含新包

--- hello-outputparser/src/index.ts
+++ hello-outputparser/src/index.ts
@@ -3,17 +3,20 @@ import { stdin as input, stdout as output } from "node:process";
 import { ChatOpenAI } from "@langchain/openai";
 import {
   JsonOutputParser,
+  StringOutputParser,
   StructuredOutputParser
 } from "@langchain/core/output_parsers";
 import { HumanMessage } from "@langchain/core/messages";
 
 import {
   ChatPromptTemplate,
+  HumanMessagePromptTemplate,
   PromptTemplate,
   SystemMessagePromptTemplate
 } from "@langchain/core/prompts";
 import { context } from "@langchain/core/utils/context";
 import z from "zod";
+import { JsonOutputToolsParser } from "@langchain/core/output_parsers/openai_tools";
 
 const readInstance = readline.createInterface({
   input,
@@ -127,36 +130,176 @@ async function jsonParser4() {
     birth: z.string().describe("出生日期，格式 YYYY-MM-DD"),
     nationality: z.string().describe("国籍"),
     achievements: z.array(z.string()).length(3).describe("成就"),
-    books: z.array(z.object({ title: z.string().describe("书名") })).max(6)
+    books: z
+      .array(
+        z.object({ title: z.string().describe("他写的书有哪些比较著名的") })
+      )
+      .max(6)
   });
 
+  const humanMessage = HumanMessagePromptTemplate.fromTemplate(`{text}`);
+
   const chatPrompt = ChatPromptTemplate.fromMessages([
-    ["system", "你是一个信息提取助手。"],
-    ["user", "{text}"]
+    ["system", "你是一个信息提取助手。{date}"],
+    humanMessage
   ]);
 
   // 2. 【核心】直接让模型绑定结构化输出能力
   // 现代大模型（如 GPT-4o, Claude 3.5, DeepSeek-V3 等）原生支持此功能
   const structuredModel = modelInstance.withStructuredOutput(structuredSchema);
 
-  //   // 3. 组装流水线 (Prompt -> 具有结构化输出能力的模型)
+  //   //   // 3. 组装流水线 (Prompt -> 具有结构化输出能力的模型)
   const chain = chatPrompt.pipe(structuredModel);
 
-  console.log("调用大模型...");
-
-  // 4. 直接调用！返回值直接就是类型安全的 JS 对象，不需要任何 Parser
+  //   // 4. 直接调用！返回值直接就是类型安全的 JS 对象，不需要任何 Parser
   const result = await chain.invoke({
+    date: Temporal.Now.plainDateISO().toLocaleString(),
     text: "介绍一下爱因斯坦的信息(中文数据)"
   });
-
-  console.log("==============================");
-  console.log(result.name); // TS 自动识别类型为 string
-  console.log(result.achievements); // TS 自动识别类型为 string[]
   console.dir(result);
 }
 
+async function jsonParser5() {
+  const structuredSchema = z.object({
+    name: z.string().describe("收货人"),
+    phoneNumber: z.number().describe("收货人手机号"),
+    areaAddress: z
+      .string()
+      .describe("所在地区，这里是一个大的区域地址，不需要包含详细地址"),
+    detailedAddress: z.string().describe("详情地址")
+  });
+
+  const humanMessage =
+    HumanMessagePromptTemplate.fromTemplate(`需要提取的信息:{text}`);
+
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    ["system", "收货人信息提取助手。{date}"],
+    humanMessage
+  ]);
+
+  // 2. 【核心】直接让模型绑定结构化输出能力
+  // 现代大模型（如 GPT-4o, Claude 3.5, DeepSeek-V3 等）原生支持此功能
+  const structuredModel = modelInstance.withStructuredOutput(structuredSchema);
+
+  //   //   // 3. 组装流水线 (Prompt -> 具有结构化输出能力的模型)
+  const chain = chatPrompt.pipe(structuredModel);
+
+  //   // 4. 直接调用！返回值直接就是类型安全的 JS 对象，不需要任何 Parser
+  const result = await chain.invoke({
+    date: Temporal.Now.plainDateISO().toLocaleString(),
+    text: "13535537651DYYYY湖南长沙市芙蓉区远大一路1149号辉煌国际城二期14栋406"
+  });
+  console.dir(result);
+}
+
+async function jsonParser6() {
+  const structuredSchema = z.object({
+    name: z.string().describe("收货人"),
+    phoneNumber: z.number().describe("收货人手机号"),
+    areaAddress: z
+      .string()
+      .describe("所在地区，这里是一个大的区域地址，不需要包含详细地址"),
+    detailedAddress: z.string().describe("详情地址")
+  });
+
+  const parser = StructuredOutputParser.fromZodSchema(structuredSchema);
+
+  const humanMessage =
+    HumanMessagePromptTemplate.fromTemplate(`需要提取的信息:{text}`);
+
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      `
+        收货人信息提取助手。{date}
+        /////////////////////////
+        格式要求(严格遵守):
+        {format}
+      `
+    ],
+    humanMessage
+  ]);
+
+  const start = chatPrompt.pipe(modelInstance).pipe(new StringOutputParser());
+
+  const result = await start.stream({
+    format: parser.getFormatInstructions(),
+    date: Temporal.Now.plainDateISO().toLocaleString(),
+    text: "13535537651李哈哈湖南长沙市芙蓉区远大一路1149号辉煌国际城二期14栋898"
+  });
+
+  for await (const chunk of result) {
+    process.stdout.write(chunk);
+  }
+}
+
+async function jsonParser7() {
+  const structuredSchema = z.object({
+    name: z.string().describe("收货人"),
+    phoneNumber: z.number().describe("收货人手机号"),
+    areaAddress: z
+      .string()
+      .describe("所在地区，这里是一个大的区域地址，不需要包含详细地址"),
+    detailedAddress: z.string().describe("详情地址")
+  });
+  const myModel = modelInstance.bindTools([
+    {
+      name: "format_consignee",
+      description: "用来提取收货人数据并且进行格式化",
+      schema: structuredSchema
+    }
+  ]);
+
+  const humanMessage =
+    HumanMessagePromptTemplate.fromTemplate(`需要提取的信息:{text}`);
+
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    ["system", `收货人信息提取助手。{date}`],
+    humanMessage
+  ]);
+
+  //   const prompt = await chatPrompt.formatMessages({
+  //     date: Temporal.Now.plainDateISO().toLocaleString("zh-cn"),
+  //     text: "13535537651李哈哈湖南长沙市芙蓉区远大一路1149号辉煌国际城二期14栋898"
+  //   });
+
+  const parser = new JsonOutputToolsParser();
+
+  console.log("开始调用大模型流水线");
+
+  const model = chatPrompt.pipe(myModel).pipe(parser);
+
+  const result = await model.stream(
+    {
+      date: Temporal.Now.plainDateISO().toLocaleString("zh-cn"),
+      text: "13535537651李哈哈湖南长沙市芙蓉区远大一路1149号辉煌国际城二期14栋898"
+    },
+    {
+      // 在这里加上 callbacks 参数拦截
+      callbacks: [
+        {
+          handleChatModelStart(llm, messages, runId, parentRunId, extraParams) {
+            console.log("\n====== 1. 最终发给大模型的文本提示词 ======");
+            console.dir(messages, { depth: null });
+
+            console.log("\n====== 2. 底层绑定的 Tools (Schema) ======");
+            // 这里能看到你的 Zod Schema 被转换成了 JSON Schema 传给了大模型
+            console.dir(extraParams?.invocation_params?.tools, { depth: null });
+
+            console.log("\n===========================================\n");
+          }
+        }
+      ]
+    }
+  );
+  console.log("返回结果");
+  for await (const chunk of result) {
+    console.log(chunk);
+  }
+}
+
 async function main() {
-  jsonParser4();
+  jsonParser7();
 }
 
 main();
